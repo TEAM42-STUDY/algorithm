c3658b8f00788422599aa964cf9187de8be16038
rudy3091 <grayblack313@gmail.com>
2021-12-25 13:25:06 +0900

    124나라의숫자 solution2 추가

--- rudy3091/124나라의숫자/index.js
+++ rudy3091/124나라의숫자/index.js
@@ -4,6 +4,16 @@ const f = (x, acc) =>
   x > 0 ? f(Math.floor((x - 1) / 3), "124"[(x - 1) % 3] + acc) : acc;
 const solution = (n) => f(n, '');
 
+function solution2(n) {
+  let answer = '';
+  while (n > 0) {
+    n -= 1;
+    answer = "124"[n % 3] + answer;
+    n = Math.floor(n / 3)
+  }
+  return answer;
+}
+
 assert.equal(solution(1), "1");
 assert.equal(solution(2), "2");
 assert.equal(solution(3), "4");
